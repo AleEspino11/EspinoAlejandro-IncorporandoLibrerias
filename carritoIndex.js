78158eed55853248ba40217f8434517bba9fe024
Ale Espino <am-espino@hotmail.com>
2022-09-14 22:41:15 -0300

    Eventos

---
+++ carritoIndex.js
@@ -0,0 +1,59 @@
+const carritoDeCompras = []
+
+const carritoIndex = (productoId)=>{
+
+    const contenedorCarrito = document.getElementById("carrito-contenedor")
+
+    const renderProductosCarrito = ()=> {
+        
+        let producto  = productos.find( producto => producto.id == productoId )
+        carritoDeCompras.push(producto)
+
+        console.log(carritoDeCompras);
+
+        producto.cantidad = 1
+      
+        contenedorCarrito.innerHTML = ""
+
+        
+        carritoDeCompras.forEach ((producto) => {
+            let div = document.createElement("div")
+            div.classList.add("productoEnCarrito")
+            div.setAttribute ("id", `div-${producto.id}`)
+
+            div.innerHTML = `<p>${producto.nombre}</p>
+                        <p>Precio: ${producto.precio}</p> 
+                        <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+                        <button id="${producto.id}" class="boton-eliminar"><i class="fa-solid fa-trash-can"></i></button>`;
+        contenedorCarrito.appendChild(div)
+
+         const eliminarCarrito = document.getElementById(producto.id)
+         const divElement = document.getElementById(`div-${producto.id}`)
+
+            console.log(eliminarCarrito);
+            eliminarCarrito.addEventListener("click", (e)=>{
+                const indice = carritoDeCompras.findIndex(element => element.id === producto.id)
+                carritoDeCompras.splice (indice,1)
+                divElement.remove()
+                /* eliminarCarrito.remove() */
+            })
+
+        } )
+       
+
+        
+    }
+
+    renderProductosCarrito()
+
+   
+} 
+
+
+/* const eliminarDelCarrito = (productoId) => {
+        const producto = carritoDeCompras.find((producto) => producto.id === productoId )
+        const indice = carritoDeCompras.indexOf (item)
+        carritoDeCompras.splice (indice, 1)
+        renderProductosCarrito()
+    } */
+
